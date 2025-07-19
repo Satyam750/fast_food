0cb862169ff9cd80ad54a520eca2388a4a862b95
Samarth Raj <samarthraj@Mac.lan>
2025-07-19 16:57:25 +0530

    auth implement and tabBar added

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,25 @@
-import { Slot } from 'expo-router'
+import { images } from '@/constants'
+import useAuthStore from '@/store/auth.store'
+import { Redirect, Slot } from 'expo-router'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
 
 const Auth_layout = () => {
+  const {isAuthenticated} = useAuthStore()
+  if(isAuthenticated) return <Redirect href={"/"}/>
+  
   return (
-   <SafeAreaView>
+  <KeyboardAvoidingView 
+  behavior={Platform.OS === 'ios'?'padding':'height'}
+  >
+    <ScrollView className='bg-white h-full' keyboardShouldPersistTaps="handled">
+      <View className='w-full relative' style={{ height: Dimensions.get('screen').height / 2.25}}>
+       <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='contain'/>
+       <Image source={images.logo} className='size-48 absolute -bottom-16 z-10 self-center'/>
+      </View>
     <Slot/>
-   </SafeAreaView>
+    </ScrollView>
+  </KeyboardAvoidingView>
   )
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,29 @@
+import useAuthStore from "@/store/auth.store";
+import * as Sentry from '@sentry/react-native';
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import "../global.css";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://4f209e6b2c0c2558032e0a497fa899d1@o4508096350912512.ingest.us.sentry.io/4509694685741056',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const {isLoading,fetchAuthenticatedUser} = useAuthStore();
+
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
     "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -16,5 +36,11 @@ export default function RootLayout() {
 if(error) throw error
 if(fontsLoaded) SplashScreen.hideAsync();
   },[fontsLoaded,error])
+
+  useEffect(()=>{
+fetchAuthenticatedUser()
+  },[])
+  if(!fontsLoaded || isLoading) return null;
+  
   return <Stack screenOptions={{headerShown:false}} />;
-}
+});
